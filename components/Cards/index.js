7f27b3d95f543822fb4942da8bf133edff9de39d
DESKTOP-J2DM1UE\Michael <michaelbarnes7282>
2020-03-13 09:17:42 -0700

    finished MVP

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,49 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(response => {
+        // console.log(Object.keys(response.data.articles));
+        Object.keys(response.data.articles).forEach(item => {
+            // console.log(response.data.articles[item]);
+            Object.keys(response.data.articles[item]).forEach(i => {
+                console.log(response.data.articles[item][i]);
+                cardsContainer.append(CreateCard(response.data.articles[item][i]));
+            })
+        })
+    })
+    .catch(error => {
+        console.log('the data was not returned', error);
+    })
+
+const cardsContainer = document.querySelector('.cards-container')
+
+function CreateCard(data) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+        const headline = document.createElement('div');
+        headline.classList.add('headline');
+        headline.textContent = data.headline;
+        card.append(headline);
+
+        const author = document.createElement('div');
+        author.classList.add('author');
+        card.append(author);
+
+            const imgContainer = document.createElement('div');
+            imgContainer.classList.add('img-container');
+            author.append(imgContainer);
+            
+                const img = document.createElement('img');
+                img.src = data.authorPhoto;
+                imgContainer.append(img);
+
+                const authorName = document.createElement('span');
+                authorName.textContent = data.authorName;
+                imgContainer.append(authorName);
+            
+    return card;
+}
